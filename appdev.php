152cb87c4560431edc5c49734db3194f8c91347a
Jin <x01Jin@users.noreply.github.com>
2023-09-23 21:13:11 +0800

    added designer using css

--- appdev.php
+++ appdev.php
@@ -1,9 +1,55 @@
 <?php
 
 print"
-lezz goo!</br>
+Task Management System</br>
 <hr></hr>
 
 ";
 
+print"
+<style>
 
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: gray;
+    text-align: center;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+}
+
+nav {
+    background-color: #444;
+    color: #fff;
+    padding: 10px;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+main {
+    padding: 20px;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
+</style>
+";
