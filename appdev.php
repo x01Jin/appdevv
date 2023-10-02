bec18a1e577dc129f48d1b5cc01f786731171b7c
Jin <x01Jin@users.noreply.github.com>
2023-10-02 18:45:56 +0800

    design marlo design part

--- appdev.php
+++ appdev.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error_message = "Invalid email or password. Please try again.";
     }
 }
-//change
+//marlo design
 echo '
 <form action="appdev.php" method="POST">
     <label for="email"><b>Email:</b></label></br>
