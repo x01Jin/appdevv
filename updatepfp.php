aa373a6e2f245a93fb220fb2ec2ce78d62933a50
Jin <x01Jin@users.noreply.github.com>
2023-10-04 00:33:36 +0800

    fixed the bug where the old pfp wont get replaced

--- updatepfp.php
+++ updatepfp.php
@@ -1,7 +1,6 @@
 <?php
 
 include_once "db.php";
-
 session_start();
 if (!isset($_SESSION["user_id"]) || $_SESSION["user_role"] !== "admin") {
     header("Location: appdev.php");
@@ -23,9 +22,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $uniqueProfilePictureName = uniqid() . '_' . $profilePictureName;
         $destinationPath = $profilePictureDirectory . $uniqueProfilePictureName;
         if (move_uploaded_file($profilePictureTmpName, $destinationPath)) {
+            $sql = "SELECT profile_picture FROM users WHERE id_number = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$user_id]);
+            $currentProfilePicture = $stmt->fetchColumn();
+
             $sql = "UPDATE users SET profile_picture = ? WHERE id_number = ?";
             $stmt = $pdo->prepare($sql);
             $stmt->execute([$uniqueProfilePictureName, $user_id]);
+
+            if ($currentProfilePicture !== "default.jpg") {
+                $oldProfilePicturePath = $profilePictureDirectory . $currentProfilePicture;
+                if (file_exists($oldProfilePicturePath)) {
+                    unlink($oldProfilePicturePath);
+                }
+            }
+
             $successMessage = "Profile picture updated successfully.";
         } else {
             $errors[] = "Failed to upload the profile picture.";
