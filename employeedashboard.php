0bb1ce7a44bbbd08101ef740f105d95135195f86
Jin <x01Jin@users.noreply.github.com>
2023-10-06 18:02:27 +0800

    forgot to remove the ?>

--- employeedashboard.php
+++ employeedashboard.php
@@ -145,4 +145,3 @@ echo '
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="script.js"></script>';
-?>
