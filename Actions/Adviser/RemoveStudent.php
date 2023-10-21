9af66ce644822c11b3036ecb0fbfafe5023ca883
Jin <x01Jin@users.noreply.github.com>
2023-10-22 00:52:11 +0800

    updated: now it also removes the associated data

--- Actions/Adviser/RemoveStudent.php
+++ Actions/Adviser/RemoveStudent.php
@@ -10,13 +10,30 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $deleteTasksStmt->bindParam(':student_id', $StudentID);
     $deleteTasksStmt->execute();
 
+    $getProfilePictureQuery = "SELECT profile_picture FROM users WHERE id_number = :student_id";
+    $getProfilePictureStmt = $pdo->prepare($getProfilePictureQuery);
+    $getProfilePictureStmt->bindParam(':student_id', $StudentID);
+    $getProfilePictureStmt->execute();
+    $profilePicture = $getProfilePictureStmt->fetchColumn();
+
+    if (!empty($profilePicture)) {
+        $profilePicturePath = "../../profile_pictures/" . $profilePicture;
+        if (file_exists($profilePicturePath)) {
+            if (unlink($profilePicturePath)) {
+                echo "Success: ";
+            } else {
+                echo "Error: ";
+            }
+        }
+    }
+
     $deleteStudentQuery = "DELETE FROM users WHERE id_number = :student_id";
     $deleteStudentStmt = $pdo->prepare($deleteStudentQuery);
     $deleteStudentStmt->bindParam(':student_id', $StudentID);
 
     if ($deleteStudentStmt->execute()) {
-        echo "Student removed and the Tasks of the student have been deleted";
+        echo "Student Removed, the tasks and the associated data of the student have been deleted.";
     } else {
-        echo "An error occurred while removing the student.";
+        echo "An error occurred while removing the student and its data.";
     }
 }
