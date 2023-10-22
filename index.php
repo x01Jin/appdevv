f0359838f909059ca90fd1344d71df9de06de04c
Jin <x01Jin@users.noreply.github.com>
2023-10-22 10:57:43 +0800

    INDEX FINAL UPDATE! (probably...)

--- index.php
+++ index.php
@@ -21,83 +21,75 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user["password"])) {
-        session_start();
-        $_SESSION["user_id"] = $user["id_number"];
-        $_SESSION["username"] = $user["full_name"];
-        $_SESSION["user_role"] = $user["role"];
-
-        if ($user["role"] == "adviser") {
-            header("Location: AccountPages/Adviser/AdviserDashboard.php");
-            exit();
-        } elseif ($user["role"] == "student") {
-            header("Location: AccountPages/Student/StudentDashboard.php");
-            exit();
-        } else {
-            header("Location: AccountPages/HeadOffice/HeadOfficeDashboard.php");
-            exit();
-        }
+        initializeSession($user);
+        redirectUser($user["role"]);
     } else {
         $error_message = "Invalid email or password. Please try again.";
     }
 }
 ?>
 
-<title>TASK.it</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TASK.it</title>
+    <style>
+        body {
+            background-image: url('assets/taskitbg.jpg');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            color: white;
+        }
+        .logo {
+            text-align: center;
+            margin-top: 20px;
+        }
+        .logo img {
+            width: 700px;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <br><br><br><br><br>
+    <div class="logo">
+        <img src="assets/logo.png" alt="Company Logo">
+    </div>
+    <br><br>
+    <form action="index.php" method="POST">
+        <label for="email"><b>Email:</b></label><br>
+        <input type="email" id="email" name="email" required><br><br>
+        <label for="password"><b>Password:</b></label><br>
+        <input type="password" id="password" name="password" required><br><br>
+        <button type="submit">Log In</button>
+    </form>
+    <br><br>
+    <?php
+    if (isset($error_message)) {
+        echo '<div style="color: red;">' . $error_message . '</div>';
+    }
+    ?>
 
-<br><br><br><br><br>
-
-<div class="logo">
-    <img src="assets/logo.png" alt="Company Logo">
-</div>
-
-<br><br>
-
-<form action="index.php" method="POST">
-    <label for="email"><b>Email:</b></label></br>
-    <input type="email" id="email" name="email" required>
-    </br></br>
-    <label for="password"><b>Password:</b></label></br>
-    <input type="password" id="password" name="password" required>
-    </br></br>
-    <button type="submit">Log In</button>
-</form>
+    <a href="registration.php" style="color:white;"><b>No Account? Register</b></a>
+</body>
+</html>
 
 <?php
-if (isset($error_message)) {
-    echo '<div style="color: red;">' . $error_message . '</div>';
+function initializeSession($user) {
+    session_start();
+    $_SESSION["user_id"] = $user["id_number"];
+    $_SESSION["username"] = $user["full_name"];
+    $_SESSION["user_role"] = $user["role"];
+}
+
+function redirectUser($role) {
+    $redirectLocation = "AccountPages/" . ucfirst($role) . "/" . $role . "Dashboard.php";
+    header("Location: $redirectLocation");
+    exit();
 }
 ?>
-
-<a href="registration.php" style="color:white;"><b>No Account? Register</b></br>
-
-<style>
-    body {
-        background-image: url('assets/taskitbg.jpg');
-        background-size: cover;
-        background-repeat: no-repeat;
-        font-family: Arial, sans-serif;
-        text-align: center;
-        color: white;
-    }
-
-    header {
-        background-color: white;
-        color: black;
-        text-align: center;
-        padding: 20px;
-    }
-
-    main {
-        padding: 20px;
-    }
-
-    .logo {
-        text-align: center;
-        margin-top: 20px;
-    }
-
-    .logo img {
-        width: 700px;
-        height: auto;
-    }
-</style>
