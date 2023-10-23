cb45cd17251c2976e7fadac36370a383efb92037
Jin <x01Jin@users.noreply.github.com>
2023-10-23 18:07:33 +0800

    updated design

--- index.php
+++ index.php
@@ -44,28 +44,75 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             font-family: Arial, sans-serif;
             text-align: center;
             color: white;
+            margin: 0;
+            padding: 0;
         }
-        .logo {
-            text-align: center;
-            margin-top: 20px;
-        }
+
         .logo img {
-            width: 700px;
+            width: 80%;
+            max-width: 400px;
             height: auto;
+        }
+
+        form {
+            max-width: 300px;
+            margin: 0 auto;
+            background: rgba(0, 0, 0, 0.5);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+
+        form label {
+            display: block;
+            text-align: center;
+            font-weight: bold;
+        }
+
+        form input {
+            width: 90%;
+            padding: 5px;
+            margin: 8px 0;
+            border: 1px solid darkblue;
+            border-radius: 5px;
+            box-sizing: border-box;
+            font-size: 16px;
+        }
+
+        form input:focus {
+            border: 1px solid lightskyblue;
+            outline: none;
+        }
+
+        form button {
+            background-color: #007BFF;
+            color: white;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        form button:hover {
+            background-color: #0056b3;
+        }
+
+        .error-message {
+            color: red;
         }
     </style>
 </head>
 <body>
-    <br><br><br><br><br>
+    <br><br><br><br><br><br>
     <div class="logo">
         <img src="assets/logo.png" alt="Company Logo">
     </div>
     <br><br>
     <form action="index.php" method="POST">
-        <label for="email"><b>Email:</b></label><br>
-        <input type="email" id="email" name="email" required><br><br>
-        <label for="password"><b>Password:</b></label><br>
-        <input type="password" id="password" name="password" required><br><br>
+        <label for="email"><b>Email:</b></label>
+        <input type="email" id="email" name="email" required><br>
+        <label for="password"><b>Password:</b></label>
+        <input type="password" id="password" name="password" required><br>
         <button type="submit">Log In</button>
     </form>
     <br><br>
